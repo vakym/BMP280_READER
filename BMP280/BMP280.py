779afb092bbf6d892a48068fbef3808c63342f1e
avakym <avakym>
2023-02-25 23:15:16 +0300

    init() added

--- BMP280/BMP280.py
+++ BMP280/BMP280.py
@@ -1,7 +1,9 @@
 import smbus
+from Registers import REGISTER
 
 BUS_NUMBER = 0
 DEVICE_ADDRESS = 0
+BMP280_CHIP_ID = 0x58
 configure = False
 bus = None
 def config(bus_number, device_address):
@@ -13,7 +15,14 @@ def config(bus_number, device_address):
 def init():
     if not configure:
         raise Exception('Not configured')
+    if bus:
+        close()
     bus = smbus.SMBus(BUS_NUMBER)
+    chipId = bus.read_byte_data(DEVICE_ADDRESS, REGISTER.BMP280_REGISTER_CHIPID)
+    if chipId != BMP280_CHIP_ID:
+        raise Exception('Not supported chip')
+    bus.write_byte_data(DEVICE_ADDRESS, REGISTER.BMP280_REGISTER_CONTROL, 0xFF)
+    bus.write_byte_data(DEVICE_ADDRESS, REGISTER.BMP280_REGISTER_CONFIG, 0x14)
 
 def close():
     bus.close()
