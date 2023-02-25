1cafe992afba4712c8282071b4b74a6b0cd461f0
avakym <avakym>
2023-02-26 00:18:52 +0300

    continue

--- Example/example1.py
+++ Example/example1.py
@@ -1,4 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from BMP280.BMP280 import BMP280
+
 
 if __name__ == "__main__":
     sensor = BMP280(1, 0x77)
